9be7556d94d5bf2d2c58f5116831bdc0a52918bc
Crackle2K <103861462+Crackle2K@users.noreply.github.com>
2026-04-29 13:48:39 -0400

    Add Tkinter login GUI and messagebox
    Import tkinter.messagebox and add a simple LoginPage class that builds a Tkinter window with frames, labels, entry fields, and a Login button. The login() handler reads the username/password and shows them via messagebox; the application is instantiated at the end and runs tk.mainloop(). Also add a pass to Savings to avoid syntax issues and adjust some method spacing/formatting in Account.

--- Unit 4 - Object Oriented Programming/U4A - Banking Application/main.py
+++ Unit 4 - Object Oriented Programming/U4A - Banking Application/main.py
@@ -5,6 +5,7 @@ Description: This is a secure online banking system program, built with Python's
 """
 
 import tkinter as tk
+from tkinter import messagebox
 
 class Account(object):
     def __init__(self, balance, accountnum, name):
@@ -14,10 +15,13 @@ class Account(object):
         
     def get_balance(self):
         return self.__balance
+    
     def set_balance(self, new_balance):
         self.__balance = new_balance
+        
     def get_accountnum(self):
         return self.__accountnum
+    
     def get_name(self):
         return self.__name
     
@@ -37,4 +41,45 @@ class Checking(Account):
         
         
 class Savings(Account):
-    
+
+class LoginPage:
+    def __init__(self):
+        self.main_window = tk.Tk()
+        self.init_frames()
+        self.init_labels()
+        self.init_buttons()
+        
+        tk.mainloop()
+        
+    def init_frames(self):
+        self.top_frame = tk.Frame()
+        self.bottom_frame = tk.Frame()
+        
+        self.top_frame.pack()
+        self.bottom_frame.pack()
+        
+    def init_buttons(self):
+        self.login_button = tk.Button(self.bottom_frame, text="Login", command=self.login)
+        
+        self.login_button.pack()
+        
+    def init_labels(self):
+        self.title = tk.Label(self.top_frame, text="Banking Application")
+        self.prompt_username = tk.Label(self.top_frame, text="Enter your username: ")
+        self.username_entry = tk.Entry(self.top_frame, width=10)
+        self.prompt_password = tk.Label(self.top_frame, text="Enter your password: ")
+        self.password_entry = tk.Entry(self.top_frame, width=10)
+        
+        self.title.pack()
+        self.prompt_username.pack()
+        self.username_entry.pack()
+        self.prompt_password.pack()
+        self.password_entry.pack()
+        
+    def login(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+        
+        messagebox.showinfo("Success", "You username is " + username + " and your password is " + password + '.')
+        
+application = LoginPage()
