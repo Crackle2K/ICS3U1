64eea9911bfe41835a9417e93401f860c9083a48
Crackle2K <103861462+Crackle2K@users.noreply.github.com>
2026-04-29 13:05:41 -0400

    Add initial main.py for banking app
    Add a new main.py scaffold for the secure online banking application. Includes authors, date, module docstring describing features (login, dashboards, account features) and imports tkinter to start GUI development.

---
+++ Unit 4 - Object Oriented Programming/U4A - Banking Application/main.py
@@ -0,0 +1,9 @@
+"""
+Authors: Dinesh Sinnathamby, Dhani Shah
+Date: April 29th, 2026
+Description: This is a secure online banking system program, built with Python's tkinter library. This program features a login system, banking dashboards, as well as a variety of different account features.
+"""
+
+import tkinter as tk
+
+
