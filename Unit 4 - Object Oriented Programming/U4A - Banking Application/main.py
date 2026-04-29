863e315e44b50156e801b4b1c4ed21f51bee5b65
dhani-shah20 <dhanishah2008@gmail.com>
2026-04-29 13:18:49 -0400

    Update main.py

--- Unit 4 - Object Oriented Programming/U4A - Banking Application/main.py
+++ Unit 4 - Object Oriented Programming/U4A - Banking Application/main.py
@@ -6,4 +6,35 @@ Description: This is a secure online banking system program, built with Python's
 
 import tkinter as tk
 
+class Account(object):
+    def __init__(self, balance, accountnum, name):
+        self.__balance = float(balance)
+        self.__accountnum = accountnum
+        self.__name = name
+        
+    def get_balance(self):
+        return self.__balance
+    def set_balance(self, new_balance):
+        self.__balance = new_balance
+    def get_accountnum(self):
+        return self.__accountnum
+    def get_name(self):
+        return self.__name
+    
+class Checking(Account):
+    def __init__(self, balance, accountnum, name):
+        Account.__init__(self, balance, accountnum, name)
 
+    def deposit(self, amount):
+        if amount > 0:
+            current = self.get_balance()
+            self.set_balance(current + amount)
+
+    def withdraw(self, amount):
+        current = self.get_balance()
+        if 0 < amount <= current:
+            self.set_balance(current - amount)   
+        
+        
+class Savings(Account):
+    
