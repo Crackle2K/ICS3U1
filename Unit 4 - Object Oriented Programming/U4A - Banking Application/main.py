e4673c07eaf1b4ddfe42653d534ce652b4dfb4b6
dhani-shah20 <dhanishah2008@gmail.com>
2026-04-30 13:32:31 -0400

    login

--- Unit 4 - Object Oriented Programming/U4A - Banking Application/main.py
+++ Unit 4 - Object Oriented Programming/U4A - Banking Application/main.py
@@ -60,6 +60,8 @@ class Savings(Account):
 class LoginPage:
     def __init__(self):
         self.main_window = tk.Tk()
+        self.main_window.title("Banking System")
+        self.main_window.geometry("400x400")
         self.init_frames()
         self.init_labels()
         self.init_buttons()
@@ -69,7 +71,6 @@ class LoginPage:
     def init_frames(self):
         self.top_frame = tk.Frame()
         self.bottom_frame = tk.Frame()
-        
         self.top_frame.pack()
         self.bottom_frame.pack()
         
@@ -86,13 +87,46 @@ class LoginPage:
         self.username_entry = tk.Entry(self.top_frame, width=15)
         self.prompt_password = tk.Label(self.top_frame, text="Enter your password: ")
         self.password_entry = tk.Entry(self.top_frame, width=15)
-        
+        self.prompt_balance = tk.Label(self.top_frame, text="Initial Deposit ($): ")
+        self.balance_entry = tk.Entry(self.top_frame, width=15)
         self.title.pack()
         self.prompt_username.pack()
         self.username_entry.pack()
         self.prompt_password.pack()
         self.password_entry.pack()
-        
+        self.prompt_balance.pack()
+        self.balance_entry.pack()
+
+    def login(self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+
+        try: 
+            database = open("userdata.txt", 'r')
+            lines = database.readlines()
+            database.close()
+            valid = False
+            for i in range(0, len(lines)):
+                stored_username = self.decrypt(lines[i].strip())
+                stored_password = self.decrypt(lines[i+1].strip())
+                
+                if username == stored_username and password == stored_password:
+                    valid = True
+                    break
+            
+            if valid:
+                self.top_frame.destroy()
+                self.bottom_frame.destroy()
+                self.show_dashboard(username)
+            else:
+                messagebox.showerror("Error", "Incorrect Username or Password")
+        except FileNotFoundError:
+            messagebox.showerror("Error", "No users registered yet!") 
+    def show_dsahboard(self, username):
+        self.main_window.title("Banking Dashboard")
+        self.main_window.geometry("400x400")
+
+
     def encrypt(self, text):
         result = ""
         for char in text:
@@ -116,18 +150,12 @@ class LoginPage:
     def sign_up(self):
         username = self.username_entry.get()
         password = self.password_entry.get()
-        
+        messagebox.showinfo("Success", "You username is " + username + " and your password is " + password + '.')
+
         database = open("userdata.txt", 'a')
         database.write(self.encrypt(username + '\n'))
         database.write(self.encrypt(password + '\n'))
         
-    def login(self):
-        username = self.username_entry.get()
-        password = self.password_entry.get()
-        
-        database = open("userdata.txt", 'r')
-        
-        
-        messagebox.showinfo("Success", "You username is " + username + " and your password is " + password + '.')
+    
         
 application = LoginPage()
