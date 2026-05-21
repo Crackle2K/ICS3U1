9fd568d62a93c139ebc0699c8bbfcd9caae41d85
Crackle2K <103861462+Crackle2K@users.noreply.github.com>
2026-05-21 14:01:49 -0400

    build: Initial commit for Unit 5 assignment

---
+++ Unit 5 - Pygame/U5A - Memory Game/main.py
@@ -0,0 +1,47 @@
+"""
+Authors: Dinesh Sinnathamby, Dhani Shah
+Date: May 21st, 2026
+Description: Complex memory game built in Pygame, featuring original sprites, opening screens, and scoring.
+"""
+
+import pygame
+
+class Main:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((640, 480))
+        pygame.display.set_caption("Memory Game v1.0.0")
+
+        self.__entities()
+
+        self.__loop()
+        
+        pygame.quit()
+        
+    def __entities(self):
+        background = pygame.Surface(self.screen.get_size())
+        self.background = background.convert()
+        self.background.fill((10, 10, 40))
+        self.screen.blit(self.background, (0, 0))
+        
+    def __loop(self):
+        clock = pygame.time.Clock()
+        self.__keep_going = True
+
+        while self.__keep_going:
+            clock.tick(30)
+            self.__handle_events()
+            self.__refresh()
+            
+    def __handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.__keep_going = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.__keep_going = False
+                    
+    def __refresh(self):
+        pygame.display.flip()
+        
+Main()
