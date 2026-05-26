39f7d8e5a5478e87d49fb62021b07cf241c118bf
dhani-shah20 <dhanishah2008@gmail.com>
2026-05-25 23:22:16 -0400

    Update main.py

--- Unit 5 - Pygame/U5A - Memory Game/main.py
+++ Unit 5 - Pygame/U5A - Memory Game/main.py
@@ -11,23 +11,18 @@ class Main:
         pygame.init()
         self.screen = pygame.display.set_mode((680, 480))
         pygame.display.set_caption("Memory Game v1.0.0")
-        cursor_image = pygame.image.load(r"Unit 5 - Pygame\U5A - Memory Game\assets\images\cursor.png")
+        cursor_image = pygame.image.load(r"img\\cursor.png")
         cursor_image = pygame.transform.scale(cursor_image, (40, 40))
-        pygame.mouse.set_cursor(pygame.Cursor((0, 0), cursor_image))
-        self.__flipped_cards = []
-        self.__waiting = False
-        self.__moves = 0
-        self.__timer_start = None
-        self.__win_time = None
-        self.__game_won = False
-        self.__game_lost = False
+        pygame.mouse.set_cursor(pygame.Cursor((0,0), cursor_image))
+        
+        
         self.__time_limit = 45
-        self.__score = 0
-
+        self.__shuffle_button_rect = pygame.Rect(520, 275, 130, 35)
+        self.__replay_button_rect = pygame.Rect(520, 325, 130, 35)
+        self.__font = pygame.font.SysFont("Arial", 22)
         self.__entities()
         self.__load_music()
         self.__load_sound_effects()
-
         self.__loop()
 
         pygame.quit()
@@ -36,13 +31,34 @@ class Main:
         background = pygame.Surface(self.screen.get_size())
         self.background = background.convert()
         self.background.fill((10, 10, 40))
-
-        self.__font = pygame.font.SysFont("Arial", 22)
+        self.__reset_game()
+                
+    def __reset_game(self):
+        self.__flipped_cards = []
+        self.__waiting = False
+        self.__moves = 0
+        self.__timer_start = None
+        self.__win_time = None
+        self.__game_won = False
+        self.__game_lost = False
+        self.__score = 0
 
         image_list = ["person.png", "ocean.png", "beach.png", "diver.png", "cave.png", "shark.png", "jellyfish.png", "pearl.png"]
         tiles = image_list * 2
         random.shuffle(tiles)
-
+        
+        self.__build_grid_from_list(tiles)
+    
+    def __shuffle_only(self):
+        if self.__game_won or self.__game_lost:
+            return
+        current_tiles = [tile.face_up_img for tile in self.all_tiles]
+        random.shuffle(current_tiles)
+        self.__flipped_cards = []
+        self.__waiting = False
+        self.__build_grid_from_list(current_tiles)
+        
+    def __build_grid_from_list(self, tile_list):
         self.all_tiles = pygame.sprite.Group()
         tilesize = 100
         margin = 10
@@ -52,17 +68,26 @@ class Main:
             for col in range(4):
                 x = 100 + col * (tilesize + margin)
                 y = 20 + row * (tilesize + margin)
-                new_tile = Tile(tiles[idx], x, y)
+                new_tile = Tile(tile_list[idx], x, y)
                 self.all_tiles.add(new_tile)
                 idx += 1
-
+                
+        
     def __load_music(self):
         try:
-            pygame.mixer.music.load(r"Unit 5 - Pygame\U5A - Memory Game\assets\sounds\aria_math.ogg")
+            pygame.mixer.music.load(r"sounds\\aria_math.ogg")
             pygame.mixer.music.play(-1)
         except:
             pass
-
+    def __load_sound_effects(self):
+        self.__snd_flip = None
+        self.__snd_match = None
+        self.__snd_win = None
+        
+        self.__snd_flip = pygame.mixer.Sound(r"sounds\\cardflip.mp3")
+        self.__snd_match = pygame.mixer.Sound(r"sounds\\matched.mp3")
+        self.__snd_win = pygame.mixer.Sound(r"sounds\\winsound.mp3")
+        
     def __loop(self):
         clock = pygame.time.Clock()
         self.__keep_going = True
@@ -72,14 +97,7 @@ class Main:
             self.__handle_events()
             self.__update()
             self.__refresh()
-    def __load_sound_effects(self):
-        self.__snd_flip = None
-        self.__snd_match = None
-        self.__snd_win = None
-        
-        self.__snd_flip = pygame.mixer.Sound(r"sounds\\cardflip.mp3")
-        self.__snd_match = pygame.mixer.Sound(r"sounds\\matched.mp3")
-        self.__snd_win = pygame.mixer.Sound(r"sounds\\winsound.mp3")
+
     def __handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -87,19 +105,34 @@ class Main:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     self.__keep_going = False
-            elif event.type == pygame.MOUSEBUTTONDOWN and not self.__waiting and not self.__game_won and not self.__game_lost:
-                for tile in self.all_tiles:
-                    if tile.rect.collidepoint(event.pos) and not tile.is_flip:
-                        if self.__timer_start is None:
-                            self.__timer_start = pygame.time.get_ticks()
-                        tile.flip()
-                        self.__snd_flip.play()
+                    
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if self.__shuffle_button_rect.collidepoint(event.pos):
+                    self.__shuffle_only()
+                    return
 
-                        self.__flipped_cards.append(tile)
-                        if len(self.__flipped_cards) == 2:
-                            self.__moves += 1
-                            self.__check_match()
-                        break
+                elif self.__replay_button_rect.collidepoint(event.pos):
+                    self.__reset_game()
+                    if not pygame.mixer.music.get_busy():
+                        self.__load_music()
+                    return
+
+                if not self.__waiting and not self.__game_won and not self.__game_lost:
+                    for tile in self.all_tiles:
+                        if tile.rect.collidepoint(event.pos) and not tile.is_flip:
+                            if self.__timer_start is None:
+                                self.__timer_start = pygame.time.get_ticks()
+                            
+                            tile.flip()
+                            if self.__snd_flip:
+                                self.__snd_flip.play()
+                                
+                            self.__flipped_cards.append(tile)
+                            if len(self.__flipped_cards) == 2:
+                                self.__moves += 1
+                                self.__check_match()
+                            break
+        
 
     def __check_match(self):
         card1, card2 = self.__flipped_cards
@@ -117,6 +150,7 @@ class Main:
             self.__win_time = pygame.time.get_ticks()
             pygame.mixer.music.stop()
             self.__snd_win.play()
+            
             elapsed = (self.__win_time - self.__timer_start)//1000
             remaining_time = max(0, self.__time_limit - elapsed)
             
@@ -125,9 +159,10 @@ class Main:
             time_bonus = remaining_time * 100
             
             self.__score = accuracy_score + time_bonus
+
     def __update(self):
-        
-        if not self.__game_won and not self.__game_lost and self.__get_elapsed == 0:
+        elapsed = self.__get_elapsed()
+        if not self.__game_won and not self.__game_lost and elapsed == 0:
             self.__game_lost = True
         
         if self.__waiting:
@@ -155,12 +190,29 @@ class Main:
 
         elapsed = self.__get_elapsed()
         moves_surf = self.__font.render(f"Moves: {self.__moves}", True, (255, 255, 255))
-        
         time_surf = self.__font.render(f"Remaining: {elapsed}s", True, (255, 255, 255))
         
         self.screen.blit(moves_surf, (530, 180))
         self.screen.blit(time_surf, (530, 225))
-
+        
+        mouse_pos = pygame.mouse.get_pos()
+        if self.__shuffle_button_rect.collidepoint(mouse_pos):
+            shuffle_color = (60, 130, 200) # Bright Blue
+        else:
+            shuffle_color = (40, 90, 160)  # Dark Blue
+        pygame.draw.rect(self.screen, shuffle_color, self.__shuffle_button_rect, border_radius=6)
+        shuffle_txt = self.__font.render("Shuffle", True, (255, 255, 255))
+        self.screen.blit(shuffle_txt, shuffle_txt.get_rect(center=self.__shuffle_button_rect.center))
+        
+        if self.__replay_button_rect.collidepoint(mouse_pos):
+            btn_color = (40, 180, 120)
+        else:
+            btn_color = (30, 140, 90)
+            
+        pygame.draw.rect(self.screen, btn_color, self.__replay_button_rect, border_radius=6)
+        btn_txt = self.__font.render("Replay", True, (255, 255, 255))
+        self.screen.blit(btn_txt, btn_txt.get_rect(center=self.__replay_button_rect.center))
+        
         if self.__game_won:
             self.__draw_win_screen(elapsed)
         elif self.__game_lost:
@@ -175,11 +227,12 @@ class Main:
 
         big_font = pygame.font.SysFont("Arial", 48)
         win_surf = big_font.render("You Win!", True, (255, 215, 0))
-        stats_surf = self.__font.render(f"Moves: {self.__moves}   Time: {elapsed}s", True, (255, 255, 255))
+        stats_surf = self.__font.render(f"Moves: {self.__moves}   Time: {45 - elapsed}s", True, (255, 255, 255))
         score_surf = big_font.render(f"Score: {self.__score}", True, (0, 255, 128))
         self.screen.blit(win_surf, win_surf.get_rect(center=(320, 210)))
         self.screen.blit(stats_surf, stats_surf.get_rect(center=(320, 270)))
         self.screen.blit(score_surf, score_surf.get_rect(center=(320, 300)))
+        
     def __draw_lose_screen(self):
         overlay = pygame.Surface(self.screen.get_size(), pygame.SRCALPHA)
         overlay.fill((0, 0, 0, 160))
@@ -200,7 +253,7 @@ class Tile(pygame.sprite.Sprite):
         self.face_up_img = face_up
         self.is_flip = False
 
-        raw_back = pygame.image.load(r"Unit 5 - Pygame\U5A - Memory Game\assets\images\back.png").convert_alpha()
+        raw_back = pygame.image.load(r"img\\back.png").convert_alpha()
         self.image = pygame.transform.scale(raw_back, (100, 100))
         
 
@@ -212,10 +265,11 @@ class Tile(pygame.sprite.Sprite):
         self.is_flip = not self.is_flip
 
         if self.is_flip:
-            raw_front = pygame.image.load(r"Unit 5 - Pygame\U5A - Memory Game\assets\images\\" + self.face_up_img).convert_alpha()
+            raw_front = pygame.image.load(r"img\\" + self.face_up_img).convert_alpha()
             self.image = pygame.transform.scale(raw_front, (100, 100))
         else:
-            raw_back = pygame.image.load(r"Unit 5 - Pygame\U5A - Memory Game\assets\images\back.png").convert_alpha()
+            raw_back = pygame.image.load(r"img\\back.png").convert_alpha()
             self.image = pygame.transform.scale(raw_back, (100, 100))
 
 Main()
+
